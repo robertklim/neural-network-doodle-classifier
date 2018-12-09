5b0dccbd8644c1cccb6e1e1178277a3ce994a9f1
Robert Klim <kontakt.robert.klim@gmail.com>
2018-12-09 18:08:55 +0100

    Load doodles from bin file and display them

--- js/sketch.js
+++ js/sketch.js
@@ -1,9 +1,37 @@
+let cats;
+let trains;
+let rainbows;
+
+function preload() {
+    cats = loadBytes('data/cats1000.bin');
+    trains = loadBytes('data/trains1000.bin');
+    rainbows = loadBytes('data/rainbows1000.bin');
+}
+
 function setup() {
-    createCanvas(600, 400);
+    createCanvas(280, 280);
+    background(0);
     
+    let total = 100;
+    for (let i = 0; i < total; i++) {
+        let img = createImage(28, 28);
+        img.loadPixels();
+        let offset = i * 784;
+        for (let j = 0; j < 784 * 4; j++) {
+            let val = 255 - cats.bytes[j + offset];
+            img.pixels[j * 4 + 0] = val;
+            img.pixels[j * 4 + 1] = val;
+            img.pixels[j * 4 + 2] = val;
+            img.pixels[j * 4 + 3] = 255;
+        }
+        img.updatePixels();
+        let x = (i % 10) * 28;
+        let y = floor(i / 10) * 28;
+        image(img, x, y);
+    }
+
 }
 
 function draw() {
-    background(0);
 
 }
